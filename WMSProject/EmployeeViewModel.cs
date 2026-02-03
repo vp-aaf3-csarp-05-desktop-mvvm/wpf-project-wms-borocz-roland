2837e82a92d20a7c6d9844957137f37507a5a5a3
c1borrol <c1borrol@vasvari.org>
2026-02-03 10:39:57 +0100

    több infó kigyűjtve

--- WMSProject/EmployeeViewModel.cs
+++ WMSProject/EmployeeViewModel.cs
@@ -3,5 +3,7 @@
     public class EmployeeViewModel
     {
         public string Name { get; } = "Nagy Anna";
+        public string AdoSzam { get; } = "383748561";
+        public int Szabadnapok { get; } = 0;
     }
 }
